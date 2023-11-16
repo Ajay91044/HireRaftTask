97579f2470fe5ec8a00316e5ea5c04229ee623fc
Apnacollege <ajay91044@gmail.com>
2023-11-16 14:08:25 +0530

    adding all the files which is required for the given task

---
+++ App.js
@@ -0,0 +1,122 @@
+// import logo from './logo.svg';
+import './App.css';
+// import TableTwo from './HireRaft/TableTwo';
+// import EnhancedTable from './HireRaft/Table';
+// import ReusableTable from './HireRaft/Table';
+import MyTable from './HireRaft/Table';
+import TableTask from './HireRaft/TableTwo';
+import TableComponent from './HireRaft/TableTwo';
+// import Table2 from './HireRaft/TableTwo';
+// import ReusableTable from './HireRaft/table2';
+// import SelectBasicExample from './HireRaft/Table';
+// import { BrowserRouter,Route,Routes } from 'react-router-dom';
+// import Products from './Component/Routing/Products';
+// import Login from './Component/Routing/Login';
+// import Signup from './Component/Routing/Signup';
+// import PageNotFound from './Component/Routing/PageNotFound';
+// import Navbar from './Component/Routing/Navbar';
+// import Home from './Component/Routing/HomePage';
+// import Mobiles from './Component/Routing/ProductsList/Mobiles';
+// import Laptops from './Component/Routing/ProductsList/Laptops';
+// import Watches from './Component/Routing/ProductsList/Watches';
+// import Televisions from './Component/Routing/ProductsList/Televisions';
+// import Table from './HireRaft/Table';
+
+function App() {
+  return (
+    <div>  
+      <MyTable/>
+      
+
+    </div>
+
+   
+    )
+  }
+  export default App;
+  
+ 
+ 
+  // <BrowserRouter>      
+  //    <Navbar  navlinks={{key1:"home",key2:"products",key3:"Login",key4:"signup"}} 
+  //    navpaths={{url1:"/",url2:"/products",url3:"/login",url4:"/signup",}} />    
+
+  //   <Routes>
+  //     <Route path="/" element={<Home/>} ></Route>
+  //     <Route path="/products" element={<Products/>} >
+  //        <Route path="mobiles" element={<Mobiles/>}></Route>
+  //        <Route path="laptops" element={<Laptops/>}></Route>
+  //        <Route path="televisions" element={<Televisions/>}></Route>
+  //        <Route path="watches" element={<Watches/>}></Route>
+  //     </Route>
+  //     <Route path="/login" element={<Login/>} ></Route>
+  //     <Route path="/signup" element={<Signup/>} ></Route>
+  //     <Route path="*" element={<PageNotFound/>} ></Route>
+  //   </Routes>
+     
+    
+  //   </BrowserRouter>  
+    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  // <div className="App">
+  
+  
+  //          <h1>Welcome Bangalore</h1>
+  //          <Add  sname="ajay" age="23">component</Add>
+  //          <Add sname="abhay" age="19"/>
+  //          <Add sname="sanjay" age="50"/>
+  
+  // </div>
+  
+  // import Count from './Component/useReducer/Count.jsx';
+  // import Todolist from './Component/practice/Todolist.jsx';
+  // import TodolistUseReducer from './Component/useReducer/TodolistUseReducer.jsx';
+  // import ToDoList from './Component/useReducer/TodolistUseReducer.jsx';
+  // import Todolist1UseReducer from './Component/useReducer/Todolist1UseReducer.jsx';
+  // import PureReactComponent from './Component/PureAndImpure/PureReactcomponent';
+// import ImpureReactComponent from './Component/PureAndImpure/ImpureReactcomponent';
+// import Formvalid from './Component/FormValidation/CustomFormValidation';
+// import CustomFormValidation from './Component/FormValidation/CustomFormValidation';
+// import Reactformhookvalidation from './Component/FormValidation/ReactFormHook.jsx/ReactformHookvalidation';
+// import Controlledcomponent from './Component/ControlledAndUncontrolled/ControlledComponent';
+// import Uncontrolledcomponent from './Component/ControlledAndUncontrolled/UncontrolledComponent';
+// import Imageslider from './Component/ImageSlider/imageslider';
+// import Addcolors from './Component/Assignments/Addcolors';
+// import Faqlist from './Component/Faq/Faqlist';
+// import Signupformdata from './Component/TaskGivenBYMam/SignupForm';
+// import CustomFormValidation from './Component/FormValidation/CustomFormValidation';
+// import ImpureReactComponent from './Component/PureAndImpure/ImpureReactcomponent';
+// import PureReactComponent from './Component/PureAndImpure/PureReactcomponent';
+// import Parentmemoisation from './Memoisation/useCallback Hook/Parentmemo';
+// import Useref from './Component/useRef/Useref';
+// import Parentmemoisation from './Memoisation/ComponentMemo/Parentmemoisation';
+// import Parentmemo from './Memoisation/usememo/Parentmemo';
+// import Moviesapi from './Api projects/MoviesApi';
+// import Fakestoreapi from './Fakestoryapi/Faskestoreapi';
+
+// import Formdata from './Form/Formdata';
+// import Counterone from './customhook/Counterone';
+// import Countertwo from './customhook/Countertwo';
+// import Fakestoreapi from './customhook/CustomHookForMovie/fakestoreapi';
+// import Githubapi from './customhook/CustomHookForMovie/githubapi';
