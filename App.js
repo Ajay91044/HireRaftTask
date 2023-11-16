6ccfbd9966a440bbd96f588b445ad1b00ab25439
Apnacollege <ajay91044@gmail.com>
2023-11-16 15:35:54 +0530

    mmodifying some codes

--- App.js
+++ App.js
@@ -1,26 +1,7 @@
-// import logo from './logo.svg';
+
 import './App.css';
-// import TableTwo from './HireRaft/TableTwo';
-// import EnhancedTable from './HireRaft/Table';
-// import ReusableTable from './HireRaft/Table';
-// import MyTable from './HireRaft/Table';
+
 import TableTask from './HireRaft/TableTwo';
-// import TableComponent from './HireRaft/TableTwo';
-// import Table2 from './HireRaft/TableTwo';
-// import ReusableTable from './HireRaft/table2';
-// import SelectBasicExample from './HireRaft/Table';
-// import { BrowserRouter,Route,Routes } from 'react-router-dom';
-// import Products from './Component/Routing/Products';
-// import Login from './Component/Routing/Login';
-// import Signup from './Component/Routing/Signup';
-// import PageNotFound from './Component/Routing/PageNotFound';
-// import Navbar from './Component/Routing/Navbar';
-// import Home from './Component/Routing/HomePage';
-// import Mobiles from './Component/Routing/ProductsList/Mobiles';
-// import Laptops from './Component/Routing/ProductsList/Laptops';
-// import Watches from './Component/Routing/ProductsList/Watches';
-// import Televisions from './Component/Routing/ProductsList/Televisions';
-// import Table from './HireRaft/Table';
 
 function App() {
   return (
@@ -35,26 +16,7 @@ function App() {
   export default App;
   
  
- 
-  // <BrowserRouter>      
-  //    <Navbar  navlinks={{key1:"home",key2:"products",key3:"Login",key4:"signup"}} 
-  //    navpaths={{url1:"/",url2:"/products",url3:"/login",url4:"/signup",}} />    
 
-  //   <Routes>
-  //     <Route path="/" element={<Home/>} ></Route>
-  //     <Route path="/products" element={<Products/>} >
-  //        <Route path="mobiles" element={<Mobiles/>}></Route>
-  //        <Route path="laptops" element={<Laptops/>}></Route>
-  //        <Route path="televisions" element={<Televisions/>}></Route>
-  //        <Route path="watches" element={<Watches/>}></Route>
-  //     </Route>
-  //     <Route path="/login" element={<Login/>} ></Route>
-  //     <Route path="/signup" element={<Signup/>} ></Route>
-  //     <Route path="*" element={<PageNotFound/>} ></Route>
-  //   </Routes>
-     
-    
-  //   </BrowserRouter>  
     
  
  
